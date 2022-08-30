618342c99eaa3961f1f35a8642c088cf11efdd10
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2022-08-30 23:10:19 +0000

    Implement board functions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,108 @@
+use std::fmt::Debug;
+
 fn main() {
-    println!("{:#?}", empty_board());
+    println!("{:#?}", empty_grid::<Piece>());
 }
 
-type Piece = [bool; 4];
+const QUATRO: usize = 4;
 
-type Board<T> = [[Option<T>; 4]; 4];
+type Piece = [bool; QUATRO];
+
+type Grid<T> = [[Option<T>; QUATRO]; QUATRO];
 
 struct Game {
-    board: Board<Piece>,
+    board: Grid<Piece>,
 }
 
-fn empty_row() -> [Option<Piece>; 4] {
-    [None; 4]
+fn empty_row<T>() -> [Option<T>; QUATRO] {
+    [None, None, None, None]
 }
 
-fn empty_board() -> Board<Piece> {
-    [empty_row(); 4]
+fn empty_grid<T>() -> Grid<T> {
+    [empty_row(), empty_row(), empty_row(), empty_row()]
 }
 
+struct Board<T> {
+    grid: Grid<T>,
+}
 
+#[derive(Debug, Copy, Clone)]
+struct Coordinate {
+    row: usize,
+    column: usize,
+}
+
+impl<T: Copy + Debug> Board<T> {
+    fn new() -> Self {
+        Board { grid: empty_grid() }
+    }
+
+    fn get(&self, position: Coordinate) -> Result<Option<T>, String> {
+        if position.row > QUATRO || position.column > QUATRO {
+            return Err(format!(
+                "Position out of bounds: you requested {position:#?} but board size is {QUATRO}"
+            ));
+        }
+        Ok(self.grid[position.column][position.row])
+    }
+
+    fn put(&mut self, piece: T, position: Coordinate) -> Result<(), String> {
+        match self.get(position)? {
+            Some(piece) => Err(format!("Place occupied by {piece:#?}")),
+            None => {
+                self.grid[position.column][position.row] = Some(piece);
+                Ok(())
+            }
+        }
+    }
+
+    fn check_if_won(&self, position: Coordinate) -> bool {
+        //     let rowItems = self.grid[position.row].into_iter().flatten().collect::<Vec<T>>();
+        //     if rowItems.len() == QUATRO {
+        //         if
+        //     }
+
+        false
+    }
+
+    fn put_and_check_if_won(&mut self, piece: T, position: Coordinate) -> Result<bool, String> {
+        match self.get(position)? {
+            Some(piece) => Err(format!("Place occupied by {piece:#?}")),
+            None => {
+                self.grid[position.column][position.row] = Some(piece);
+                Ok(self.check_if_won(position))
+            }
+        }
+    }
+
+    fn remove(&mut self, position: Coordinate) -> Result<(), String> {
+        match self.get(position)? {
+            Some(_) => {
+                self.grid[position.column][position.row] = None;
+                Ok(())
+            }
+            None => Err("Place is empty".to_string()),
+        }
+    }
+
+    fn empty_spaces(&self) -> Vec<Coordinate> {
+        let mut result = Vec::new();
+
+        for (row_index, row) in self.grid.iter().enumerate() {
+            for (column_index, value) in row.iter().enumerate() {
+                match value {
+                    None => result.push(Coordinate {
+                        column: column_index,
+                        row: row_index,
+                    }),
+                    Some(_) => (),
+                }
+            }
+        }
+
+        result
+    }
+}
 
 // impl Game {
 //     fn new() -> Game {

c565dc4b01f3bc8dee6b8f0673798443b60eeb58
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2022-09-03 19:58:46 +0000

    Add optimizations

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() -> Result<(), String> {
         ([false, true, true, false], (1, 2)),
         ([true, true, false, false], (2, 0)),
         ([true, true, false, true], (2, 1)),
-        ([true, true, true, false], (2, 2)),
+        // ([true, true, true, false], (2, 2)),
     ];
 
     for (piece, (row, column)) in pieces_with_coordinates {
@@ -373,6 +373,11 @@ where
     fn actions(&self, state: &State) -> Vec<Action>;
     fn result(&self, state: &State, action: Action) -> State;
 
+    // Minimax will handle 3 possible values as result of the game:
+    // 1 Player 1 wins
+    // -1 Player 2 wins
+    // 0 Draw
+    // This information will be used to do optimizations
     fn min_value(&self, state: &State) -> i32 {
         if self.terminal(state) {
             return self.utility(state);
@@ -381,6 +386,9 @@ where
         let mut v = i32::MAX;
         for action in self.actions(state) {
             v = v.min(self.max_value(&self.result(state, action)));
+            if v == -1 {
+                return v;
+            }
         }
 
         v
@@ -393,6 +401,9 @@ where
         let mut v = i32::MIN;
         for action in self.actions(state) {
             v = v.max(self.min_value(&self.result(state, action)));
+            if v == 1 {
+                return v;
+            }
         }
 
         v
